ef1e6d4421d4a8c85d231dde0c5fe0f2087e7e4a
Nicolas <bernalmedina.n@gmail.com>
2022-01-06 20:43:22 -0500

    manejo de routerlink id

--- src/app/heroes/pages/heroe/heroe.component.ts
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-heroe',
@@ -8,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroeComponent implements OnInit {
 
-  constructor() { }
+  constructor( private acttivatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+
+    this.acttivatedRoute.params.subscribe( ({id}) => console.log(id));
+
   }
 
 }
