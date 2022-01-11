989590388bb3e3fb05ea08ffcb127f13153f2f4c
Nicolas <bernalmedina.n@gmail.com>
2022-01-11 16:07:23 -0500

    ajuste de enviroments http

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { Heroe } from '../../interfaces/heroes.interface';
 
 @Injectable({
@@ -8,17 +9,17 @@ import { Heroe } from '../../interfaces/heroes.interface';
 })
 export class HeroesService {
 
-  ruta: string = "";
+  private baseUrl: string = environment.baseURL;
 
   constructor(private http: HttpClient) { 
   }
 
   getHeroes(): Observable<Heroe[]> {
-    return this.http.get<Heroe[]>("http://localhost:3000/heroes")
+    return this.http.get<Heroe[]>(`${this.baseUrl}/heroes`)
   }
 
   getHeroePorId( id:string ): Observable<Heroe> {
-    return this.http.get<Heroe>(`http://localhost:3000/heroes/${ id }/`)
+    return this.http.get<Heroe>(`${this.baseUrl}/heroes/${ id }/`)
   }
 
 }
